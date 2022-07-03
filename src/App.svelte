555395c9d4a3bc492309b7a29c70b110cb6fb85b
MGDSoft <miguelgd@mgdsoftware.es>
2022-07-04 01:29:35 +0200

    logout option + keep browser history

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,24 @@
 <script>
-	import { executeGraphql } from "./utils.js";
-	import { userInfo } from './store'
 
-	import Keycloak from "keycloak-js";
-	let kc = new Keycloak("/keycloak.json");
+  import {userInfo} from './store'
+  import Keycloak from "keycloak-js";
 
-	let logged_in = null;
+  let kc = new Keycloak({
+    url: 'http://localhost:8080/', realm: 'master', clientId: 'app'
+  });
 
-	let todos = [];
+  let logged_in = null;
 
-	kc.init({ onLoad: "check-sso" }).then((auth) => {
-		logged_in = auth;
-		if (auth) {
-			logged_in = true;
+  kc.init({onLoad: "check-sso"}).then((auth) => {
+    if (auth) {
+      logged_in = true;
 
-			kc.loadUserInfo().then((user) => {
-				user.token = kc.idToken
-				userInfo.set(user)
-			});
-		}
-	});
+      kc.loadUserInfo().then((user) => {
+        user.token = kc.idToken
+        userInfo.set(user)
+      });
+    }
+  });
 
 </script>
 
@@ -27,22 +26,38 @@
 
 {#if logged_in && $userInfo.preferred_username}
 
-	<pre>{JSON.stringify($userInfo, null,2)}</pre>
-	You are logged in as {$userInfo.preferred_username}
+    <pre>{JSON.stringify($userInfo, null, 2)}</pre>
 
-	<button
-		on:click={() => {
-			kc.logout();
-		}}>Logout</button
-	>
+    You are logged in as {$userInfo.preferred_username}
+
+    {kc.createLogoutUrl() + '?id_token_hint=' + $userInfo.token + '&post_logout_redirect_uri=' + encodeURIComponent(window.location.href)}
+    <button
+            on:click={() => {
+			window.location.href=kc.createLogoutUrl() + '&id_token_hint=' + encodeURIComponent($userInfo.token) + '&post_logout_redirect_uri=' + encodeURIComponent(window.location.href);
+		}}>Logout
+    </button>
+
+    <button
+            on:click={() => {
+			window.location.href=kc.createAccountUrl();
+		}}> account
+    </button>
 
 {/if}
 
-{#if logged_in == false}
-	You are not logged in
-	<button
-		on:click={() => {
-			kc.login();
-		}}>Login</button
-	>
+{#if !logged_in }
+    You are not logged in
+    <button
+            on:click={() => {
+			window.location.href=kc.createLoginUrl();
+		}}>Login
+    </button>
+
+    try to register
+    <button
+            on:click={() => {
+			window.location.href=kc.createRegisterUrl();
+		}}>Register
+    </button>
+
 {/if}
